69b22b39cbf3938c4c492afc219b2e9a0bed747c
Jezreel Maldonado <jezreel.maldonado@upr.edu>
2022-05-17 00:42:03 -0400

    removed unnecesary import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { TopBarModule } from './modules/top-bar/top-bar.module';
 import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ParticipantsAddComponent } from './participants/participants-add/participants-add.component';
 import { ParticipantsModule } from './participants/participants.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
